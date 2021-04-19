90d421ce1c0c8fbd207428b677cacefed956e094
kavinda-ravishan <kvnravishan@outlook.com>
2021-04-19 19:41:02 +0530

    App init with structure

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
