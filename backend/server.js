712dc607ecc28a2aad50f565eb1f876e1e4cfff9
kavinda-ravishan <kvnravishan@outlook.com>
2021-04-20 21:53:11 +0530

    mongoDB connection and import shop data

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ dotenv.config();
 const express = require("express");
 const app = express();
 
+const connectDB = require("./config/db");
+
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
