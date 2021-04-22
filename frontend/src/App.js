7bbacc0bfa448fb63c9b2a7deb49343850bbc622
kavinda-ravishan <kvnravishan@outlook.com>
2021-04-22 22:15:41 +0530

    screens and componets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//Screens
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
-    <div className="app">
+    <Router>
       {/*Navbar*/}
       {/*Side drawer*/}
       {/*Backdrop*/}
-      {/*Home screen*/}
-      {/*Product screen*/}
-      {/*CartScreen*/}
-    </div>
+      <main>
+        <Switch>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/product/:id" component={ProductScreen} />
+          <Route exact path="/cart" component={CartScreen} />
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
