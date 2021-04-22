62035757558ff2bf9db7dd2cec169ddf5824b6c9
kavinda-ravishan <kvnravishan@outlook.com>
2021-04-22 22:53:06 +0530

    navbar style

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -9,10 +9,12 @@ const Navbar = () => {
       </div>
       <ul className="navbar__links">
         <li>
-          <Link to="/cart">
+          <Link to="/cart" className="cart__link">
             <i className="fas fa-shopping-cart"></i>
-            Cart
-            <span className="cartlogo__badge">0</span>
+            <span>
+              Cart
+              <span className="cartlogo__badge">0</span>
+            </span>
           </Link>
         </li>
         <li>
