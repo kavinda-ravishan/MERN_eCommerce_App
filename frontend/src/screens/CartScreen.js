d5a8119f252bb9ef38831d1c07c769bb93de8507
kavinda-ravishan <kvnravishan@outlook.com>
2021-04-27 18:22:50 +0530

    cart screen style

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -9,13 +9,20 @@ const CartScreen = () => {
       <div className="cartscreen__left">
         <h2>Shopping Cart</h2>
         <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
       </div>
       <div className="cartscreen__right">
         <div className="cartscreen__info">
           <p>Subtotal (0) items</p>
           <p>$499.99</p>
         </div>
-        <button>Proceed To Checkout</button>
+        <div>
+          <button>Proceed To Checkout</button>
+        </div>
       </div>
     </div>
   );
